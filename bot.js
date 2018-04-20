cf70f3dbea971de80dbd39be46c1f21ed6a71aca
Tromodolo <Cazionchallosette@gmail.com>
2018-04-20 22:13:35 +0200

    Update db

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const eris = require("eris"),
-    fs = require("fs"),
-    db = require("./db");
-      
+	fs = require("fs"),
+	db = require("./db");
+	
 var config = require("./config.json"),
 	customCommands = require("./data/customcommands.json"),
 	rolesCommand = require("./data/rolescommand.json"),
 	color = 0xd83e3e,
-    commandList = [];
+	commandList = [];
 
 var exports = module.exports = {};
 var messageTimers = [];
@@ -53,7 +53,7 @@ fs.readdir("./commands/", (err, folders) => {
 			});
 			commandList.push(commandModule);
 			console.log(`Loaded ${loadedFiles} commands in module ${folderName}`);    
-            
+			
 			commandList.sort(function(a, b){
 				if (a.name < b.name)
 					return -1;
@@ -69,88 +69,80 @@ fs.readdir("./commands/", (err, folders) => {
 
 bot.on("ready", () => { // When the bot is ready
 	console.log("Successfully connected as: " + bot.user.username + "#" + bot.user.discriminator); // Log "Ready!"
-    bot.editStatus({ name: "!help to see all commands"});
-    db.AssignRoles.sync();
-    db.Config.sync();
-    db.CustomCommands.sync();
-    db.UserLevels.sync();
+	bot.editStatus({ name: "!help to see all commands"});
+	db.AssignRoles.sync();
+	db.Config.sync();
+	db.CustomCommands.sync();
+	db.UserLevels.sync();
 });
 
 bot.on("messageCreate", async (message) => {
 	if(message.author.bot)
 		return;
 
-    let customCommandList = await db.CustomCommands.findAll({ raw: true });
-    
-    let custom = customCommandList.find(x => x.commandname == message.content)
-    if(custom){
-        bot.createMessage(message.channel.id, custom.commandtext);        
-    }
+	let customCommandList = await db.CustomCommands.findAll({ raw: true });
+	
+	let custom = customCommandList.find(x => x.commandname == message.content);
+	if(custom){
+		bot.createMessage(message.channel.id, custom.commandtext);        
+	}
 
-    let now = new Date();
-    let xpGain = getRandomInt(15, 26);
-    
-    if(await checkifUserTimer(message.author.id)){
-        let oldTime = await getTimer(message.author.id);
-        if((((now - oldTime) / 1000) > 60)) {
-            await addExp(message, xpGain);
-            await updateTimer(message.author.id, now); 
-            
-        }
-        else{
-            return;
-        }
-    } 
-    else{
-        await addExp(message, xpGain);        
-        await addTimer(message.author.id, now);
-    }
+	let now = new Date();
+	let xpGain = getRandomInt(15, 26);
+
+	if(await checkifUserTimer(message.author.id)){
+		let oldTime = await getTimer(message.author.id);
+		if((((now - oldTime) / 1000) > 60)) {
+			await addExp(message, xpGain);
+			await updateTimer(message.author.id, now); 
+			
+		}
+		else{
+			return;
+		}
+	} 
+	else{
+		await addExp(message, xpGain);        
+		await addTimer(message.author.id, now);
+	}
 });
 
 
 bot.on("messageReactionAdd", async (message, emoji, userID) =>{
-    let config = await db.Config.find(
-    { 
-        where:{
-            id: 1
-        },
-        raw: true
-    });
+	let config = await db.Config.find(
+		{ 
+			where:{
+				id: 1
+			},
+			raw: true
+		});
 
-    let rolescommand = await db.AssignRoles.findAll({
-        raw: true
-    });
-    
-    if(!config.reactionmessageid){
-        return;
-    }
-    if(message.id !== config.reactionmessageid){
-        return;
-    }
-    else{
-        var member = message.channel.guild.members.find(x => x.id == userID);
-        if(member.bot){
-            return;
-        }
-        else{
-            var role = rolescommand.find(x => x.emoteid == emoji.id);
-            member.addRole(role.roleid, "Roles Command");
-        } 
-    }
+	let rolescommand = await db.AssignRoles.findAll({
+		raw: true
+	});
+	
+	if(!config.reactionmessageid){
+		return;
+	}
+	if(message.id !== config.reactionmessageid){
+		return;
+	}
+	else{
+		var member = message.channel.guild.members.find(x => x.id == userID);
+		if(member.bot){
+			return;
+		}
+		else{
+			var role = rolescommand.find(x => x.emoteid == emoji.id);
+			member.addRole(role.roleid, "Roles Command");
+		} 
+	}
 });
 
 bot.on("messageReactionRemove", async (message, emoji, userID) =>{
-    let config = await db.Config.find(
-        { 
-            where:{
-                id: 1
-            },
-            raw: true
-        });
-    
-        let rolescommand = await db.AssignRoles.findAll({
-            raw: true
-        });
+	let rolescommand = await db.AssignRoles.findAll({
+		raw: true
+	});
 
 	if(!rolescommand.reactionmessageid){
 		return;
@@ -177,101 +169,104 @@ bot.on("messageReactionRemove", async (message, emoji, userID) =>{
 });
 
 function checkifUserTimer( userid ){
-    for (var i in messageTimers) {
-        if (messageTimers[i].userid === userid) {
-            return true;
-        }
-    }
-    return false;
+	for (var i in messageTimers) {
+		if (messageTimers[i].userid === userid) {
+			return true;
+		}
+	}
+	return false;
 }
 
 async function addTimer( userid, newTime ){
-    await messageTimers.push({ "userid": userid, "time": newTime });
+	await messageTimers.push({ "userid": userid, "time": newTime });
 }
 
 function updateTimer( userid, newTime){
-    for (var i in messageTimers) {
-        if (messageTimers[i].userid == userid) {
-            messageTimers[i].time = newTime;
-        }
-    }
+	for (var i in messageTimers) {
+		if (messageTimers[i].userid == userid) {
+			messageTimers[i].time = newTime;
+		}
+	}
 }
 
 function getTimer( userid ){
-    for (var i in messageTimers) {
-        if (messageTimers[i].userid == userid) {
-            return messageTimers[i].time;
-        }
-    }
-    return null;
+	for (var i in messageTimers) {
+		if (messageTimers[i].userid == userid) {
+			return messageTimers[i].time;
+		}
+	}
+	return null;
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
 async function addExp(message, xpGain){
-    let user = await db.UserLevels.findOrCreate(
-    { 
-        raw: true,
-        where:{
-            userid: message.author.id
-        },
-        defaults:{
-            userid: message.author.id,
-            username: message.author.username,
-            discriminator: message.author.discriminator,
-            avatarurl: message.author.avatarURL,
-            totalxp: 0,
-            currentxp: 0,
-            level: 0
-        }
-    });
-    
-    let currentlevel = user.level;
-    let newLevel, restExp;
-    let levelxp = ((5 * Math.pow(currentlevel, 2)) + (40 * currentlevel) + 55);
+	let user = await db.UserLevels.findOrCreate(
+		{ 
+			raw: true,
+			where:{
+				userid: message.author.id
+			},
+			defaults:{
+				userid: message.author.id,
+				username: message.author.username,
+				discriminator: message.author.discriminator,
+				avatarurl: message.author.avatarURL,
+				totalxp: 0,
+				currentxp: 0,
+				level: 0
+			}
+		});
 
-    //If they leveled up
-    if(levelxp <= user.currentxp + xpGain){
-        //Set exp to what's left after leveling up
-        restExp = xpGain + (user.currentxp % levelxp);
-        //Increase level
-        newLevel = currentlevel + 1;
-    }
+	user = user[0];
 
-    if(newLevel && restExp){
-        //Means they leveled up
-        await db.UserLevels.update(
-        {
-            level: newLevel,
-            currentxp: restExp,
-            totalxp: db.sequelize.literal(`totalxp + ${xpGain}`),
-            avatarurl: message.author.avatarURL
-        }, 
-        {
-            where: {
-                userid: message.author.id
-            }
-        });
-        await bot.createMessage(message.channel.id, message.author.mention + " just achieved level **" + user.level + "**!");                         
-    }
-    else{
-        await db.UserLevels.update(
-        {
-            level: newLevel,
-            currentxp: db.sequelize.literal(`currentxp + ${xpGain}`),
-            totalxp: db.sequelize.literal(`totalxp + ${xpGain}`),
-            avatarurl: message.author.avatarURL
-        }, 
-        {
-            where: {
-                userid: message.author.id
-            }
-        });
-    }
+	let currentlevel = user.level;
+	let newLevel, restExp;
+	let levelxp = ((5 * Math.pow(currentlevel, 2)) + (40 * currentlevel) + 55);
+	
+	//If they leveled up
+	if(levelxp <= user.currentxp + xpGain){		
+		//Set exp to what's left after leveling up
+		restExp = xpGain + (user.currentxp - levelxp);
+		//Increase level
+		newLevel = currentlevel + 1;
+	}
+
+	if(newLevel && restExp){
+		
+		//Means they leveled up
+		await db.UserLevels.update(
+			{
+				level: newLevel,
+				currentxp: restExp,
+				totalxp: db.sequelize.literal(`totalxp + ${xpGain}`),
+				avatarurl: message.author.avatarURL
+			}, 
+			{
+				where: {
+					userid: message.author.id
+				}
+			});
+		//await bot.createMessage(message.channel.id, message.author.mention + " just achieved level **" + user.level + "**!");                         
+	}
+	else{		
+		await db.UserLevels.update(
+			{
+				level: newLevel,
+				currentxp: db.sequelize.literal(`currentxp + ${xpGain}`),
+				totalxp: db.sequelize.literal(`totalxp + ${xpGain}`),
+				avatarurl: message.author.avatarURL
+			}, 
+			{
+				where: {
+					userid: message.author.id
+				}
+			});
+	}
 }
 
 bot.connect();
--- db.js
+++ db.js
@@ -1,47 +1,49 @@
 var exports = module.exports = {};
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('kurisu', 'tromodolo', 'Mitsuka123', {
-    host: 'tromo.me',
-    dialect: 'mysql',
-    operatorsAliases: false,
-  
-    pool: {
-      max: 20,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    define:{
-        timestamps: false,
-        freezeTableName: true
-    },
-    
-     // disable logging; default: console.log enable if need be
-    logging: false
+const sequelize = new Sequelize("kurisu", "tromodolo", "Mitsuka123", {
+	host: "tromo.me",
+	dialect: "mysql",
+	operatorsAliases: false,
+
+	pool: {
+		max: 20,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	},
+	define:{
+		timestamps: false,
+		freezeTableName: true,
+		charset: "utf8mb4",
+		collate: "utf8mb4_bin"
+	},
+	
+	// disable logging; default: console.log enable if need be
+	logging: false
 });
 
-const AssignRoles = sequelize.define('assignroles', {
+const AssignRoles = sequelize.define("assignroles", {
 	roleid: {
-        type: Sequelize.STRING,
-        primaryKey: true
-    },
-    emotename: {
-        type: Sequelize.STRING
-    },
-    emoteid: {
-        type: Sequelize.STRING
-    },
-    description: {
-        type: Sequelize.STRING(2000)
-    }
+		type: Sequelize.STRING,
+		primaryKey: true
+	},
+	emotename: {
+		type: Sequelize.STRING
+	},
+	emoteid: {
+		type: Sequelize.STRING
+	},
+	description: {
+		type: Sequelize.STRING(2000)
+	}
 });
 
-const UserLevels = sequelize.define('userlevels', {
+const UserLevels = sequelize.define("userlevels", {
 	userid: {
-	  type: Sequelize.STRING,
-      primaryKey: true
+		type: Sequelize.STRING,
+		primaryKey: true
 	},
 	username: {
 		type: Sequelize.STRING
@@ -60,29 +62,33 @@ const UserLevels = sequelize.define('userlevels', {
 	},
 	level: {
 		type: Sequelize.INTEGER
+	},
+	profiledescription:{
+		type: Sequelize.STRING,
+		defaultValue: "I haven't changed this"
 	}
 });
 
-const Config = sequelize.define('config', {
-    id:{
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    reactionmessageid: {
-        type: Sequelize.STRING
-    }
+const Config = sequelize.define("config", {
+	id:{
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true
+	},
+	reactionmessageid: {
+		type: Sequelize.STRING
+	}
 });
 
-const CustomCommands = sequelize.define('customcommands',{
-    commandname:{
-        type: Sequelize.STRING,
-        primaryKey: true
-    },
-    commandtext: {
-        type: Sequelize.STRING(2000)
-    }
+const CustomCommands = sequelize.define("customcommands",{
+	commandname:{
+		type: Sequelize.STRING,
+		primaryKey: true
+	},
+	commandtext: {
+		type: Sequelize.STRING(2000)
+	}
 });
 
 exports.sequelize = sequelize;
