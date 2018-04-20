7f837df3555dede908138c0de0274eaecf983b66
Tromodolo <Cazionchallosette@gmail.com>
2018-04-20 23:31:32 +0200

    Fix profile command

---
+++ commands/other/profile.js
@@ -0,0 +1,28 @@
+var exports = module.exports = {},
+	client = require("../../bot.js").client,
+	color = require("../../bot.js").kurisuColour,
+	request = require("request"),
+	fs = require("fs");
+
+const download = require("image-downloader");
+	
+exports.function = async (msg, args) => {
+	let options = {
+		url: `https://tromo.me/leblanc/getprofile.php?id=${msg.author.id}`,
+		dest: `./data/tmp/discordavatars/${msg.author.id}.png`        // Save to /path/to/dest/photo.jpg
+	};
+	
+	download.image(options)
+		.then(({ filename, image }) => {
+			fs.readFile(`./data/tmp/discordavatars/${msg.author.id}.png`, function (err, data ) {
+				msg.channel.createMessage("", { file: data, name: "profile.png" });
+			});
+		}).catch((err) => {
+			throw err;
+		});		
+	
+};
+
+exports.description = "Shows info about a discord emote";
+exports.fullDescription = "Shows information about a custom discord emote";
+exports.usage = "<emote>";
