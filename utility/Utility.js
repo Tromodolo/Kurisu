1b55a27b4fd62b1bd8256d9527b11f228347b913
Tromodolo <Cazionchallosette@gmail.com>
2018-07-14 12:14:32 +0200

    Add getUserByMessage to Utility

--- utility/Utility.js
+++ utility/Utility.js
@@ -161,5 +161,58 @@ module.exports.DiscordEmbed = class DiscordEmbed{
 
 		return fullEmbed;
 	}
-
 };
+
+module.exports.getUserByMessage = (msg, args) => {
+	let user;
+	
+	if(args.length == 0){
+		user = msg.member;
+	}
+
+	let mentionRegex = /(<@!?[0-9]*>)(?:\s?\w*)*/gi;
+
+	let mentionCheck = mentionRegex.exec(args.join(" "));
+
+	if(mentionCheck){
+		user = msg.mentions[0];
+
+		//Workaround to get member object instead of just user
+		user = msg.channel.guild.members.find(x => x.id == user.id);
+	}
+	else if(!user){
+		let guild = msg.channel.guild;
+		user = guild.members.find(x => x.id == args[0]);
+
+		if(!user){
+			user = guild.members.find(x => x.username.toLowerCase() == args[0]);
+			if(!user){
+				user = guild.members.find(x => {
+					if(x.nick){
+						return x.nick.toLowerCase() == args[0];
+					}
+					else{
+						return false;
+					}
+				});
+				if(!user){
+					user = guild.members.find(x => x.username.toLowerCase().includes(args[0]));
+					if(!user){
+						user = guild.members.find(x => {
+							if(x.nick){
+								return x.nick.toLowerCase().includes(args[0]);
+							}
+							else{
+								return false;
+							}
+						});
+						if(!user){
+							return null;
+						}
+					}
+				}
+			}
+		}
+	}
+	return user;
+}
