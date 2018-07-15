a4db81430c0323d87220c21411d5b37dd14cca20
Tromodolo <Cazionchallosette@gmail.com>
2018-07-15 17:56:50 +0200

    Add image love command

--- utility/Utility.js
+++ utility/Utility.js
@@ -168,6 +168,7 @@ module.exports.getUserByMessage = (msg, args) => {
 	
 	if(args.length == 0){
 		user = msg.member;
+		return;
 	}
 
 	let mentionRegex = /(<@!?[0-9]*>)(?:\s?\w*)*/gi;
@@ -215,4 +216,92 @@ module.exports.getUserByMessage = (msg, args) => {
 		}
 	}
 	return user;
+}
+
+
+module.exports.getLoveUsers = (msg, args) => {
+	let users = { first: null, second: null };
+	
+	if(args.length == 0){
+		return users;
+	}
+
+	let firstuser = msg.mentions[0];
+	if(firstuser) users.first = msg.channel.guild.members.find(x => x.id == firstuser.id);
+
+	let seconduser = msg.mentions[1];
+	if(seconduser) users.second = msg.channel.guild.members.find(x => x.id == seconduser.id);
+
+	let guild = msg.channel.guild;
+
+	if(!users.first){
+		users.first = guild.members.find(x => x.id == args[0]);
+
+		if(!users.first){
+			users.first = guild.members.find(x => x.username.toLowerCase() == args[0]);
+
+			if(!users.first){
+				users.first = guild.members.find(x => {
+					if(x.nick){
+						return x.nick.toLowerCase() == args[0];
+					}
+					else{
+						return false;
+					}
+				});
+				if(!users.first){
+					users.first = guild.members.find(x => x.username.toLowerCase().includes(args[0]));
+					if(!users.first){
+						users.first = guild.members.find(x => {
+							if(x.nick){
+								return x.nick.toLowerCase().includes(args[0]);
+							}
+							else{
+								return false;
+							}
+						});
+						if(!users.first){
+							users.first = null;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if(!users.second){
+		users.second = guild.members.find(x => x.id == args[1]);
+
+		if(!users.second){
+			users.second = guild.members.find(x => x.username.toLowerCase() == args[1]);
+
+			if(!users.second){
+				users.second = guild.members.find(x => {
+					if(x.nick){
+						return x.nick.toLowerCase() == args[1];
+					}
+					else{
+						return false;
+					}
+				});
+				if(!users.second){
+					users.second = guild.members.find(x => x.username.toLowerCase().includes(args[1]));
+					if(!users.second){
+						users.second = guild.members.find(x => {
+							if(x.nick){
+								return x.nick.toLowerCase().includes(args[1]);
+							}
+							else{
+								return false;
+							}
+						});
+						if(!users.second){
+							users.second = null;
+						}
+					}
+				}
+			}
+		}
+	}
+	return users;
 }
