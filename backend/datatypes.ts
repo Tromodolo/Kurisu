ab6d87ff0d4bd19617d73e135671a5a493876ec4
Tromodolo <Cazionchallosette@gmail.com>
2018-10-09 20:36:38 +0200

    Update backend to have an api endpoint for user experience
    Endpoint is /api/user/experience

---
+++ backend/datatypes.ts
@@ -0,0 +1,9 @@
+interface UserExperienceUpdate{
+	userid: string;
+	leveledUp: boolean;
+	level: number;
+}
+
+export {
+	UserExperienceUpdate,
+};
--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,15 @@
 // Import everything from express and assign it to the express variable
+import bodyParser from "body-parser";
 import express from "express";
 import db from "../db";
-
-// Import WelcomeController from controllers entry point
 import {UserController} from "./controllers";
 
 // Create a new express application instance
 const app: express.Application = express();
 // The port the express app will listen on
 const port: number = 7600;
+// parse application/json
+app.use(bodyParser.json());
 
 // Api routes
 app.use("/api/user", UserController);
