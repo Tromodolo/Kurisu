135157514e5a7de9be792a394d37af7b07016dec
Elias Mawa <emawa@sawol.moe>
2018-10-08 19:07:49 -0600

    added in depth JSDoc comments for the types
    Added JSDoc comments for Command, and CommandModule Interfaces. UserTimer needs to be fixed up though.

--- types.ts
+++ types.ts
@@ -1,9 +1,26 @@
 import { Message } from "eris";
 
+/**
+ * Interface for a command object
+ *
+ * @interface Command
+ * @prop {string} name The name of the command
+ * @prop {string} description A short description of the command
+ * @prop {string} fullDescription A more in depth description of the command
+ * @prop {string} Basic usage instructions for the command
+ * @prop {string[]} aliases Other names for the command
+ * @prop {object} List of requirements needed for the command
+ * @prop {boolean} Flag used to determine wether to delete the uder message
+ */
 interface Command {
 	name: string;
 	description: string;
 	fullDescription: string;
+	/**
+	* @arg {Message} message The message sent
+	* @arg {String[]} args Array of all the args sent with the command
+	* @returns {void}
+	*/
 	function: (message: Message, args: string[]) => Promise<{}>;
 	usage: string;
 	aliases: string[];
@@ -11,11 +28,25 @@ interface Command {
 	deleteCommand: boolean;
 }
 
+/**
+ * Interface for Command groups
+ *
+ * @interface CommandModule
+ * @prop {string} name The group that the particular command belongs to
+ * @prop {Command[]} commands Array of all the commands within the command group
+ */
 interface CommandModule {
 	name: string;
 	commands: Command[];
 }
 
+/**
+ * Interface for object that represents {TODO}
+ *
+ * @interface UserTimer
+ * @prop {string} userid {TODO}
+ * @prop {number} time {TODO}
+ */
 interface UserTimer {
 	userid: string;
 	time: number;
