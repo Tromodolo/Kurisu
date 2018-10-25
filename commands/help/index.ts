2b5f5ff2136cd03c3cc7116b6016579cbda09482
Tromodolo <Cazionchallosette@gmail.com>
2018-10-25 21:00:32 +0200

    Add entrypoints for all modules

---
+++ commands/help/index.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { Command, CommandModule } from "../../types";
+
+class Module extends CommandModule{
+	public name: string = "Help";
+	public commands: Command[] = [];
+	public permissions: string[] = [];
+
+	constructor(){
+		super();
+		fs.readdir(path.join(__dirname, "./"), (folderErr, files) => {
+			for (const file of files){
+				if (!(file === "index.ts")){
+					const command: Command = require(path.join(__dirname, `/${file}`));
+					this.commands.push(command);
+				}
+			}
+		});
+	}
+}
+
+export {
+	Module,
+};
