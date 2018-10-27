c1595c55d0af0027ae12f672188a99672e8ed05b
Tromodolo <Cazionchallosette@gmail.com>
2018-10-28 01:26:43 +0200

    Update types to add comments and change Command to class

--- types.ts
+++ types.ts
@@ -3,38 +3,74 @@ import fs from "fs";
 import path from "path";
 
 /**
- * Interface for a command object
+ * Class definition for a command object
  *
- * @interface Command
+ * @class Command
  * @prop {string} name The name of the command
  * @prop {string} description A short description of the command
  * @prop {string} fullDescription A more in depth description of the command
- * @prop {string} Basic usage instructions for the command
+ * @prop {string} usage Basic usage instructions for the command
  * @prop {string[]} aliases Other names for the command
- * @prop {object} List of requirements needed for the command
- * @prop {boolean} Flag used to determine wether to delete the uder message
+ * @prop {object} requirements List of requirements needed for the command
+ * @prop {boolean} deleteCommand Flag used to determine whether to delete the under message
+ * @prop {function} commandFunc Command function that runs when the command gets triggered
  */
-interface Command {
-	commandName: string;
-	description: string;
-	fullDescription: string;
+class Command {
+	public commandName: string = "";
+	public description: string = "";
+	public fullDescription: string = "";
+	public usage: string = "";
+	public aliases: string[] = [];
+	public requirements: string[] = [];
+	public deleteCommand: boolean = false;
+
+	/**
+	 * @param name The name of the command
+	 * @param description A short description of the command
+	 * @param fullDesc A more in depth description of the command
+	 * @param usage Usage instructions for the command
+	 * @param aliases Other names for the command
+	 * @param requirements List of requirements needed for the command
+	 * @param deleteCommand Flag used to determine whether to delete the under message
+	 * @param func Command function that runs when the command gets triggered
+	 */
+	constructor(name: string, description: string, fullDesc: string, usage: string,
+				aliases: string[], requirements: string[], deleteCommand: boolean,
+				func: (message: eris.Message, args: string[]) => Promise<{}>){
+		this.commandName = name;
+		this.description = description;
+		this.fullDescription = fullDesc;
+		this.usage = usage;
+		this.aliases = aliases;
+		this.requirements = requirements;
+		this.deleteCommand = deleteCommand;
+		this.commandFunc = func;
+	}
+
 	/**
 	 * @arg {Message} message The message sent
 	 * @arg {string[]} args Array of all the args sent with the command
 	 * @returns {Promise<{}>}
 	 */
-	commandFunc: (message: eris.Message, args: string[]) => Promise<{}>;
-	usage: string;
-	aliases: string[];
-	requirements: string[];
-	deleteCommand: boolean;
+	public commandFunc: (message: eris.Message, args: string[]) => Promise<{}> = () => {
+		return new Promise((resolve) => resolve());
+	}
+
+	public checkPermissions(permissions: eris.Permission): boolean {
+		for (const perm of this.requirements){
+			if (!permissions.has(perm)){
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 /**
- * Interface for Command groups
+ * Class definition for Command Modules
  *
  * @class CommandModule
- * @prop {string} name The group that the particular command belongs to
+ * @prop {string} name The module that the particular command belongs to
  * @prop {Command[]} commands Array of all the commands within the command group
  * @prop {string[]} permissions Array of all the different permissions needed
  */
@@ -43,6 +79,11 @@ class CommandModule {
 	public commands: Command[] = [];
 	public permissions: string[] = [];
 
+	/**
+	 * @param name The module that the particular command belongs to
+	 * @param permissions Array of all the commands within the command module
+	 * @param commandPath Array of all the different permissions needed
+	 */
 	constructor(name: string, permissions: string[], commandPath: string){
 		this.name = name;
 		this.permissions = permissions;
@@ -50,13 +91,18 @@ class CommandModule {
 		fs.readdir(path.join(commandPath, "./"), (folderErr, files) => {
 			for (const file of files){
 				if (!(file === "index.ts")){
-					const command: Command = require(path.join(commandPath, `/${file}`));
-					this.commands.push(command);
+					const command = require(path.join(commandPath, `/${file}`));
+					this.commands.push(command.default);
 				}
 			}
 		});
 	}
 
+	/**
+	 * Checks whether or not the passed through permissions are allowed to run a command from a module
+	 * @param permissions eris.js permissions object
+	 * @returns {boolean}
+	 */
 	public checkPermissions(permissions: eris.Permission): boolean{
 		for (const perm of this.permissions){
 			if (!permissions.has(perm)){
@@ -66,6 +112,11 @@ class CommandModule {
 		return true;
 	}
 
+	/**
+	 * Checks to see whether or not a command exists in a module. Returns undefined if not found
+	 * @param name Name of command to find
+	 * @returns {undefined | Command}
+	 */
 	public findCommand(name: string): undefined | Command {
 		let command;
 		for (const com of this.commands){
