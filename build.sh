5a1fb700f0a70ccec5b75a62c5e168773cd26c16
Elias Mawa <emawa@sawol.ca>
2019-03-31 11:11:03 -0600

    add run functionality, fix build dir location error, fix duplicate dir error.

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
-tsc
-mkdir build/data
-cp -R src/data build/data
-pause
+# build.sh - utlities for building and running kurisubot
+#
+# ./build.sh build - transpile code, and move to the build/ dir
+# ./build.sh run - run kurisubot
+#
+
+if [ "$1" = "build" ] # transpile the typescript
+then
+	tsc
+	# create dir if not exists then copy transpiled files
+	mkdir -p build/data && cp -R src/data build/
+elif [ "$1" = "run" ] # run program
+then
+	# move to bot dir and run bot
+	cd build && node bot.js
+fi
