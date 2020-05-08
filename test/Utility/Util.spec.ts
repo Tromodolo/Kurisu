f9fed7d779095e3086ed100258b2b4c56410ee4f
Filip Ekström <Filip.ekstrom98@gmail.com>
2020-05-08 23:07:15 +0200

    Remove a Util test cause of it being async and super slow

--- test/Utility/Util.spec.ts
+++ test/Utility/Util.spec.ts
@@ -150,12 +150,4 @@ describe('Utility Functions', () => {
 			expect(channel?.id).to.equal("1");
 		});
 	});
-
-	it('Getting image from url', () => {
-		return new Promise(async (resolve) => {
-			const color = await getPrimaryColorFromImageUrl("https://tro.moe/img/HonkConfused.png");
-			expect(typeof color).to.equal("number");
-			return resolve();
-		});
-	});
 });
